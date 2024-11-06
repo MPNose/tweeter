e84da8544faedb20878c40abb89fab01a472e39a
MPNose <mpnoseworthy89@gmail.com>
2024-11-06 14:57:35 -0800

    create ajax function in client.js to send post request to server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,13 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+$("form").on('submit', function(event) {
+  event.preventDefault();
+  const data = $(this).serialize();
+  $.ajax({
+    method: 'POST',
+    url: "/tweets",
+    data: data
+  })
+console.log(data);
+});
