defe3e4d2c2b11c2a62c06fb05f19d03f1e3af9a
MPNose <mpnoseworthy89@gmail.com>
2024-11-06 19:51:33 -0800

    create loadTweets function, and implement timeago script

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,6 +3,19 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  function loadTweets() {
+    $.ajax('/tweets', {
+      method: 'GET'
+    })
+    .then(function(data) {
+      renderTweets(data);
+    })
+  }
+
+  loadTweets();
+});
+  
 $("form").on('submit', function(event) {
   event.preventDefault();
   const data = $(this).serialize();
@@ -11,43 +24,21 @@ $("form").on('submit', function(event) {
     url: "/tweets",
     data: data
   })
-console.log(data);
-});
+})
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
+
+
 
 const renderTweets = function(tweets) {
   for (const tweet of tweets) {
-    console.log(tweet);
     let renderTweet = createTweetElement(tweet);
-    $('.tweet-container').append(renderTweet);
+    $('.tweet-container').prepend(renderTweet);
   }
 };
 
 const createTweetElement = function(tweet) {
+  const date = tweet.created_at;
   let $tweet = $(`<article class="tweet-class">
     <header>
       <div>
@@ -58,7 +49,7 @@ const createTweetElement = function(tweet) {
       </header>
         <p class="tweet">${tweet.content.text}</p>
         <footer>
-          <p>${tweet.created_at}</p>
+          <p>${timeago.format(date)}</p>
           <p><i class="fa-solid fa-flag"></i>  <i class="fa-solid fa-retweet"></i> 
              <i class="fa-solid fa-heart"> </i></p>
         </footer>
@@ -66,4 +57,3 @@ const createTweetElement = function(tweet) {
   return $tweet;
 };
 
-renderTweets(data);
